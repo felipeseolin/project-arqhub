66edb274d55b5877df2e3adc2db30cc845e21d0e
Felipe Seolin Bento <felipe.seolin@hotmail.com>
2019-06-07 16:32:29 -0300

    New user info at database
    project-arqhub-13

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,11 @@ class CreateUsersTable extends Migration {
 				$table->string('email')->unique();
 				$table->timestamp('email_verified_at')->nullable();
 				$table->string('password');
+				$table->date('birth')->nullable();
+				$table->integer('phone')->nullable();
+				$table->boolean('active')->default(true);
+				$table->string('photo')->nullable();
+				$table->text('bio')->nullable();
 				$table->rememberToken();
 				$table->timestamps();
 			});

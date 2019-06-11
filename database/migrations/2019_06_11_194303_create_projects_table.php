830fda2a92dbddecc034150910fea6a65910489c
Felipe Seolin Bento <felipe.seolin@hotmail.com>
2019-06-11 16:56:30 -0300

    Migration Created

---
+++ database/migrations/2019_06_11_194303_create_projects_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->text('description');
+            $table->date('date');
+            $table->decimal('area');
+            $table->integer('num_bedrooms');
+            $table->integer('num_bathrooms');
+            $table->integer('num_floors');
+            $table->integer('num_parking');
+	        $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+	
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}

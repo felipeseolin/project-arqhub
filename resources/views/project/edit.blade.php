d05e72a6f175b26d7712e1a9f99b951bc64ab6f7
Elian Marcos <elians.2016@alunos.utfpr.edu.br>
2019-06-23 12:18:13 -0300

    Alterações layouts

--- resources/views/project/edit.blade.php
+++ resources/views/project/edit.blade.php
@@ -3,10 +3,12 @@
 @section('content')
     @include('navbar')
 
-    <section id="edit" class="container">
-        <h1>Novo Projeto</h1>
-        <p>Alguma outra coisa</p>
-
+    <section id="edit-project" class="container border">
+        <div class="border">
+        <h1 class="text-center font-weight-bold">{{$project->name}}</h1>
+        <p class="text-center font-italic">Aqui você irá editar as especificações do seu projeto, confira tudo antes de salvar...</p>
+        </div>
+        
         @if(isset($update))
             <div class="alert {!! $update ? "alert-success" : "alert-danger"  !!}" role="alert">
                 {{ $msg }}
@@ -17,78 +19,105 @@
             {!! method_field('PUT') !!}
 
             <div class="form-group">
-                <label for="name">Nome do projeto</label>
+                <label class="font-weight-bold" for="name">Nome</label>
                 <input id="name" name="name" class="form-control" type="text" minlength="5" maxlength="250"
                        value="{{$project->name}}" required>
-            </div>
+            </div >
 
             <div class="form-group">
-                <label for="category">Categoria</label>
+                <label class="font-weight-bold" for="description">Descrição</label>
+                <textarea id="description" name="description" class="form-control" rows="5">{{$project->description}}</textarea>
+            </div>
+
+           
+
+            <div class="form-row">
+
+            <div class="form-group row">
+                <label class="font-weight-bold col-sm-3 col-form-label" for="area">Área</label>
+                <div class="col-sm-7">
+                <input id="area" name="area" class="form-control" type="number" min="10" value="{{$project->area}}" required>
+                </div>
+            </div>
+
+             
+            <div class="form-group row">
+                <label class="font-weight-bold col-sm-3 col-form-label" for="num_bedrooms"> Quartos</label>
+                <div class="col-sm-7">
+                <input id="num_bedrooms" name="num_bedrooms" class="form-control" type="number" min="0" value="{{$project->num_bedrooms}}" required>
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <label class="font-weight-bold col-sm-3 col-form-label" for="num_bathrooms">Banheiros</label>
+                <div class="col-sm-9">
+                <input id="num_bathrooms" name="num_bathrooms" class="form-control" type="number" min="0" value="{{$project->num_bathrooms}}"  required>
+                </div>
+            </div>
+
+           
+            </div>
+
+            <div class="form-row">
+           
+            <div class="form-group row">
+                <label class="font-weight-bold col-sm-3 col-form-label" for="num_suites">Suítes</label>
+                <div class="col-sm-7">
+                <input id="num_suites" name="num_suites" class="form-control" type="number" min="0" value="{{$project->num_suites}}" required>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="font-weight-bold col-sm-3 col-form-label" for="num_floors">Andares</label>
+                <div class="col-sm-7">
+                <input id="num_floors" name="num_floors" class="form-control" type="number" min="0" value="{{$project->num_floors}}" required>
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <label class="font-weight-bold col-sm-3 col-form-label" for="num_parking">Vagas de Estacionamento</label>
+                <div class="col-sm-6">
+                <input id="num_parking" name="num_parking" class="form-control" type="number" min="0" value="{{$project->num_parking}}" required>
+                </div>
+            </div>
+
+           
+            </div>
+            <div class="form-row">
+            <div class="form-group row">
+                <label class="font-weight-bold col-sm-3 col-form-label" for="width">Largura</label>
+                <div class="col-sm-7">
+                <input id="width" name="width" class="form-control" type="number" min="0" value="{{$project->width}}" required>
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <label class="font-weight-bold col-sm-3.5 col-form-label" for="length">Comprimento</label>
+                <div class="col-sm-6">
+                <input id="length" name="length" class="form-control" type="number" min="0" value="{{$project->length}}" required>
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <label class="font-weight-bold col-sm-4 col-form-label" for="category">Categoria</label>
+                <div class="col-sm-8">
                 <select id="category" name="category" class="form-control" required>
                     @foreach($categories as $category)
-                        <option id="{{$category}}" name="{{$category}}" value="{{ $category }}"
+                    <option id="{{$category}}" name="{{$category}}" value="{{ $category }}"
                                 {{ $category == $project->category ? "selected" : "" }}
-                        >{{ $category }}</option>
-                    @endforeach
+                        >{{ $category }}</option>                    @endforeach
                 </select>
+                </div>
+
+            </div>
             </div>
 
-            <div class="form-group">
-                <label for="description">Descrição</label>
-                <textarea id="description" name="description" class="form-control" rows="5">{{$project->description}}
-                </textarea>
-            </div>
-
-            <div class="form-group">
-                <label for="area">Área em m²</label>
-                <input id="area" name="area" class="form-control" type="number" min="10" value="{{$project->area}}"
-                       required>
-            </div>
-
-            <div class="form-group">
-                <label for="num_bedrooms">Quantidade de quartos</label>
-                <input id="num_bedrooms" name="num_bedrooms" class="form-control" type="number" min="0"
-                       value="{{$project->num_bedrooms}}" required>
-            </div>
-
-            <div class="form-group">
-                <label for="num_bathrooms">Quantidade de banheiros</label>
-                <input id="num_bathrooms" name="num_bathrooms" class="form-control" type="number" min="0"
-                       value="{{$project->num_bathrooms}}" required>
-            </div>
-
-            <div class="form-group">
-                <label for="num_suites">Quantidade de suítes</label>
-                <input id="num_suites" name="num_suites" class="form-control" type="number" min="0"
-                       value="{{$project->num_suites}}" required>
-            </div>
-
-            <div class="form-group">
-                <label for="num_floors">Quantidade de andares</label>
-                <input id="num_floors" name="num_floors" class="form-control" type="number" min="0"
-                       value="{{$project->num_floors}}" required>
-            </div>
-
-            <div class="form-group">
-                <label for="num_parking">Quantidade de vagas de estacionamento</label>
-                <input id="num_parking" name="num_parking" class="form-control" type="number" min="0"
-                       value="{{$project->num_parking}}" required>
-            </div>
-
-            <div class="form-group">
-                <label for="width">Largura em metros</label>
-                <input id="width" name="width" class="form-control" type="number" min="0"
-                       value="{{$project->width}}" required>
-            </div>
-
-            <div class="form-group">
-                <label for="length">Comprimento em metros</label>
-                <input id="length" name="length" class="form-control" type="number" min="0"
-                       value="{{$project->length}}" required>
-            </div>
-
-            <button id="submit" class="btn btn-success" type="submit">Salvar</button>
+            <button id="submit" class="btn btn-success btn-lg" type="submit">Salvar</button>
+            
         </form>
-
     </section>
+    
+       
+    
+    
+    
 @endsection

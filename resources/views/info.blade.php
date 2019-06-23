5d6acb0f8f994c9a3d94faa4ce726178bfbdd08d
Elian Marcos <elians.2016@alunos.utfpr.edu.br>
2019-06-23 13:40:09 -0300

    Alteração  info

--- resources/views/info.blade.php
+++ resources/views/info.blade.php
@@ -4,21 +4,31 @@
     @include('navbar')
 
     <section id="info" class="container">
-        <h1>Project ArqHub</h1>
-        <p>
-            Projeto de software realizado para a disciplina de Oficina de Integração 2, do curso de Engenharia de
-            Software na UTFPR (Universidade Tecnológica Federal do Paraná), câmpus Cornélio Procópio.
+        <div class='border'>
+        <h1 class="text-center font-weight-bold">Project ArqHub</h1>
+        <p class="text-center font-italic">
+            Projeto de software realizado para a disciplina de Oficina de Integração 2<br/> Curso Bach. em Engenharia de
+            Software<br/>UTFPR - Universidade Tecnológica Federal do Paraná<br/>Câmpus Cornélio Procópio.
         </p>
-
-        <h2>Alunos</h2>
-        <ul>
-            <li>Elian Marcos Veloso</li>
-            <li>Felipe Seolin Bento</li>
-            <li>Rafael Fernandes Marques</li>
-            <li>Wirlley Oliveira Delfino</li>
-        </ul>
-
-        <h2>Professor</h2>
-        <p>Rogerio Santos Pozza</p>
+        </div>
+        <div class='border'>
+        <h2 class="text-center font-weight-bold">Alunos</h2>
+        
+            <p class="text-center font-italic ">Elian Marcos Veloso</p>
+            <p class="text-center font-italic">Felipe Seolin Bento</p>
+            <p class="text-center font-italic">Rafael Fernandes Marques</p>
+            <p class="text-center font-italic">Wirlley Oliveira Delfino</p>
+        
+        </div>
+        <div >
+        <h2 class="text-center font-weight-bold">Professor</h2>
+        <p class="text-center font-italic">Rogerio Santos Pozza</p>
+        </div>
+        
     </section>
+    <div><p><br/><br/><br/><br/></p></div>
+    @include('footer')
+
+
+
 @endsection

ff724b137216b4869aa37f05960b83f8c5a5df55
Felipe Seolin Bento <felipe.seolin@hotmail.com>
2019-11-15 22:27:36 -0300

    Adição do email da equipe

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -4,7 +4,8 @@
           Copyright © ArqHub 2019
         </p>
         <p class="m-0 text-center text-white">
-            <a class="nav-link" href="{{url('info')}}">Sobre o projeto</a>
+            <a class="nav-link" href="{{url('info')}}" style="white-space: nowrap;display: inline" >Sobre o projeto</a>
+            <a class="nav-link" href="mailto:projectarqhub@gmail.com" style="white-space: nowrap;display: inline">Dúvidas/Ajuda</a>
         </p>
     </div>
 </footer>
